58e6a54fe352f8c79ea9483fabfc035da78e5f5a
mpicard <martin8768@gmail.com>
2018-09-28 17:31:10 -0400

    Fix body margin on prod

--- src/components/text-input.js
+++ src/components/text-input.js
@@ -1,21 +1,24 @@
-import { Component } from 'react';
+import React from 'react';
 
-class TextInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-  }
+const Capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
-  handleChange(event) {
-    const { value } = event.target;
-    this.setState({ value });
-  }
-
-  render() {
-    return (
-      <input value={this.state.value} onChange={e => this.handleChange(e)} />
-    );
-  }
-}
+const TextInput = ({ name, children }) => (
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/6">
+      <label
+        htmlFor={name}
+        className="block text-grey-darker font-bold mb-1 md:mb-0 pr-4">
+        {Capitalize(name)}
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      {children ? (
+        children
+      ) : (
+        <input type="text" className="w-full" name={name} />
+      )}
+    </div>
+  </div>
+);
 
 export default TextInput;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-
-const Cap = str => str.charAt(0).toUpperCase() + str.slice(1);
-
-const TextInput = ({ name, children }) => (
-  <div className="md:flex md:items-center mb-6">
-    <div className="md:w-1/6">
-      <label
-        htmlFor={name}
-        className="block text-grey-darker font-bold mb-1 md:mb-0 pr-4">
-        {Cap(name)}
-      </label>
-    </div>
-    <div className="md:w-2/3">
-      {children ? (
-        children
-      ) : (
-        <input type="text" className="w-full" name={name} />
-      )}
-    </div>
-  </div>
-);
+import TextInput from '../components/text-input';
 
 const SecondPage = () => (
   <Layout>
