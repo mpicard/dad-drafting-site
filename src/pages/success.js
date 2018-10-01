cdacca41cda2a73c79d8a0b3b43790c363177475
mpicard <martin8768@gmail.com>
2018-10-01 15:03:44 -0400

    Add success page

---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const SuccessPage = () => (
+  <Layout>
+    <p className="text-teal text-xl font-bold">Success!</p>
+    <p>Thanks for contacting us, we'll get back to you as soon as possible.</p>
+  </Layout>
+);
+
+export default SuccessPage;
